309efdaba90bc57a1ff53897db7d99834cc2cba5
marchel99 <kubamarchelski@live.com>
2023-05-18 23:49:32 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   zad1.3.py

--- zad1.3.py
+++ zad1.3.py
@@ -1,19 +1,28 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 
-# Tworzenie drzewa za pomocą NetworkX
+# Tworzenie pustego obiektu grafu
 G = nx.DiGraph()
 
-# Dodanie węzłów
-G.add_node("Problem: Błędne wyniki dla niektórych kombinacji wejść")
-G.add_node("Hipoteza 1: Problem z wejściem E")
-G.add_node("Hipoteza 2: Problem z wyjściem W1")
+# Dodawanie krawędzi do grafu
+G.add_edge('T4', 'T31')
+G.add_edge('T4', 'T20')
 
-# Dodanie krawędzi
-G.add_edge("Problem: Błędne wyniki dla niektórych kombinacji wejść", "Hipoteza 1: Problem z wejściem E")
-G.add_edge("Problem: Błędne wyniki dla niektórych kombinacji wejść", "Hipoteza 2: Problem z wyjściem W1")
+G.add_edge('T20', 'T12')
+G.add_edge('T20', 'Test Vector')
 
-# Rysowanie drzewa
+G.add_edge('T31', 'Failed Test')
+ 
+G.add_edge('Failed Test', 'T21')
+
+G.add_edge('T21', 'Failed Test')
+G.add_edge('T21', 'Test Vector')
+
+G.add_edge('Failed Test', 'E stuck-at-0')
+
+# Rysowanie grafu
 pos = nx.spring_layout(G)
-nx.draw(G, pos, with_labels=True, node_color='lightblue', edge_color='gray', node_size=3000, font_size=12, font_weight='bold')
+nx.draw(G, pos, with_labels=True, node_color='lightblue', edge_color='gray')
+
+# Wyświetlanie grafu
 plt.show()
