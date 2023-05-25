ea040ab1ea885871a81ba640399f902d93cd4bf1
Jakub Marchelski <71104830+marchel99@users.noreply.github.com>
2023-05-25 11:57:11 +0200

    dodana druga czesc zadania

--- zad2_b.py
+++ zad2_b.py
@@ -1,18 +1,17 @@
 """
 Znaleźć miejsce zerowe podanej funkcji w zadanym przedziale wykorzystując metodę
-złotego podziału
-a) Dla osób z parzystą cyfrą I2:
-f(x)=x2 –x -2-I1 w przedziale [-5,5],
+
+f(x)=sin(x*I2) cos(x)/(1-x*x) w przedziale [-5,5],
+Dla kryterium stopu eps=0.001 (abserror).
 """
-import math
 import numpy as np
 import matplotlib.pyplot as plt
 
 def f(x):
-    return x**2 - x - 2
+    return (np.sin(x**2) * np.cos(x)) / (1 - x**2)
 
 def golden_section_search(a, b, epsilon):
-    ratio = (math.sqrt(5) - 1) / 2  # Stosunek złotego podziału
+    ratio = (np.sqrt(5) - 1) / 2  # Stosunek złotego podziału
 
     while abs(b - a) > epsilon:
         c = b - ratio * (b - a)
@@ -27,18 +26,23 @@ def golden_section_search(a, b, epsilon):
 
     return (a + b) / 2
 
+# Przedział początkowy
 a = -5
 b = 5
 
-epsilon = 0.0001
+# Dokładność (kryterium stopu)
+epsilon = 0.001
 
+# Znalezienie miejsca zerowego
 zero_point = golden_section_search(a, b, epsilon)
 zero_point_value = f(zero_point)
 
-x = np.linspace(-6, 6, 100)
+# Generowanie punktów do wykresu funkcji
+x = np.linspace(-5, 5, 1000)
 y = f(x)
 
-plt.plot(x, y, label='f(x) = x^2 - x - 2')
+# Wykres funkcji
+plt.plot(x, y, label='f(x) = sin(x^2) * cos(x) / (1 - x^2)')
 plt.axhline(y=0, color='black', linestyle='--')
 plt.axvline(x=zero_point, color='red', linestyle='--', label='Miejsce zerowe')
 plt.scatter(zero_point, 0, color='red')
@@ -47,10 +51,12 @@ plt.ylabel('f(x)')
 plt.title('Wykres funkcji')
 plt.legend()
 
-plt.text(-7, 52, 'Nr indeksu: 249842, I2=4', fontsize=12)
-plt.text(-7, 49, 'Przedział: [-5, 5]', fontsize=12)
-plt.text(-7, 46, 'Miejsce zerowe: x = {:.4f}'.format(zero_point), fontsize=12 )
+# Dodanie linijek opisu na górze wykresu
+plt.text(-7, 78, 'Nr indeksu: 249842, I2=4', fontsize=12)
+plt.text(-7, 73, 'Przedział: [-5, 5]', fontsize=12)
+plt.text(-7, 68, 'Miejsce zerowe: x = {:.4f}'.format(zero_point), fontsize=12)
 
+# Wyświetlenie wykresu
 plt.show()
 
 print("Miejsce zerowe:", zero_point)
