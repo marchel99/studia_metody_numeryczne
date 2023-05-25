830c33b6cb8f369023fcc3e85fbe01f47b9ba45b
Jakub Marchelski <71104830+marchel99@users.noreply.github.com>
2023-05-25 12:21:53 +0200

    zad3b

---
+++ zad3_b.py
@@ -0,0 +1,69 @@
+"""
+Wyznacz miejsce zerowe, metoda Brenta (złoty podział z interpolacją f.
+kwadratową), wielomian 3. stopnia (dla różnych punktów startowych);
+
+f(x)=sin(x*I2) cos(x)/(1-x*x) w przedziale [-5,5],
+Dla kryterium stopu eps=0.001 (abserror).
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import brentq
+
+def f(x):
+    return np.sin(x**2) * np.cos(x) / (1 - x**2)
+
+# Przedział początkowy
+a = -5
+b = 5
+
+# Kryterium stopu
+epsilon = 0.001
+
+# Znalezienie miejsc zerowych
+zero_points = []
+x = a
+
+while x <= b:
+    try:
+        zero_point = brentq(f, x, b, xtol=epsilon)
+        zero_points.append(zero_point)
+        x = zero_point + epsilon
+    except ValueError:
+        x += epsilon
+
+# Wykres funkcji
+x_vals = np.linspace(a, b, 100)
+y_vals = f(x_vals)
+
+plt.plot(x_vals, y_vals, label='f(x) = sin(x^2) * cos(x) / (1 - x^2)')
+plt.axhline(y=0, color='black', linestyle='--')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title('Wykres funkcji')
+plt.legend()
+
+# Oznaczenie miejsc zerowych
+if len(zero_points) > 0:
+    for i, zero_point in enumerate(zero_points):
+        zero_point_value = f(zero_point)
+        plt.scatter(zero_point, 0, color='red')
+        if i == 1:
+            plt.text(zero_point, 0.85, 'x = {:.4f}'.format(zero_point), fontsize=8, ha='center')
+        else:
+            plt.text(zero_point, 0.5, 'x = {:.4f}'.format(zero_point), fontsize=8, ha='center')
+
+        print("Miejsce zerowe:", zero_point)
+        print("Wartość funkcji w miejscu zerowym:", zero_point_value)
+        print()
+else:
+    print("Brak miejsc zerowych w danym przedziale.")
+
+# Dodatkowe informacje
+plt.text(-7, 9, 'Nr indeksu: 249842, I2=4', fontsize=12)
+plt.text(-7, 8, 'Przedział: [-5, 5]', fontsize=12)
+if len(zero_points) > 0:
+    plt.text(-7, 7, 'Miejsce zerowe: x = {:.4f}'.format(zero_points[0]), fontsize=12)
+
+# Wyświetlenie wykresu
+plt.show()
