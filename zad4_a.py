150bf68dfc43ff28a103de1c8772045164a29af2
Jakub Marchelski <71104830+marchel99@users.noreply.github.com>
2023-05-25 14:47:26 +0200

    d

--- zad4_a.py
+++ zad4_a.py
@@ -4,78 +4,75 @@ przypadku;
 Dany jest układ równań:
 {
 𝑥1 = 3𝑥2 + 𝐼1
-𝑥2 = 𝑥1
-3 − 𝐼2
-gdzie x=[x1,x2]
-T
-.
-Punkt startowy: x0=[1,1]T
+𝑥2 = 𝑥13 − 𝐼2
+
+gdzie x=[x1,x2]^T
+Punkt startowy: x0=[1,1]^T
 Dla kryterium stopu eps=0.001 (abserror).
+
 """
 
 
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from scipy import optimize
 
-def f(x):
-    x1 = x[0]
-    x2 = x[1]
-    eq1 = x1 - 3*x2 - 2
-    eq2 = x2 - x1**3 + 4
-    return np.array([eq1, eq2])
+# Definiowanie funkcji
+def F(X):
+    x1, x2 = X
+    return [x1 - 3*x2 - 2, x2 - x1**3 + 4]
 
-def jacobian(x):
-    x1 = x[0]
-    x2 = x[1]
-    jacobian_matrix = np.array([[1, -3], [-3*x1**2, 1]])
-    return jacobian_matrix
-
-def newton_method(f, jacobian, x0, epsilon, max_iterations):
-    x = x0
-    iterations = 0
-
-    while np.linalg.norm(f(x)) > epsilon and iterations < max_iterations:
-        delta_x = np.linalg.solve(jacobian(x), -f(x))
-        x = x + delta_x
-        iterations += 1
-
-    if iterations == max_iterations:
-        print("Nie udało się znaleźć rozwiązania w zadanej liczbie iteracji.")
-
-    return x
+# Definiowanie Jacobianu (macierzy pochodnych cząstkowych)
+def jacobian(X):
+    x1, x2 = X
+    return [[1, -3], [-3*x1**2, 1]]
 
 # Punkt startowy
-x0 = np.array([1, 1])
+X0 = [1, 1]
 
-# Kryterium stopu
-epsilon = 0.001
+# Metoda Newtona-Raphsona
+for i in range(100):  # max liczba iteracji
+    delta = np.linalg.solve(jacobian(X0), F(X0))  # obliczenie delta X
+    X0 -= delta  # aktualizacja punktu startowego
+    if np.linalg.norm(delta) < 0.001:  # sprawdzenie kryterium stopu
+        break
 
-# Maksymalna liczba iteracji
-max_iterations = 100
+solution = X0
 
-# Rozwiązanie układu równań metodą Newtona
-solution_newton = newton_method(f, jacobian, x0, epsilon, max_iterations)
+# Definiujemy nasze funkcje
+def f1(x1, x2):
+    return x1 - 3*x2 - 2
 
-# Rozwiązanie układu równań metodą Newtona-Raphsona
-solution_newton_raphson = newton_method(f, jacobian, x0, epsilon, max_iterations)
+def f2(x1, x2):
+    return x2 - x1**3 + 4
 
-# Wykres
-x1_vals = np.linspace(-5, 5, 100)
-x2_vals = np.linspace(-5, 5, 100)
-X1, X2 = np.meshgrid(x1_vals, x2_vals)
-F = f([X1, X2])
-fig, ax = plt.subplots()
-ax.contour(X1, X2, F[0], levels=[0], colors='red', linestyles='dashed')
-ax.contour(X1, X2, F[1], levels=[0], colors='blue', linestyles='dashed')
-ax.scatter(solution_newton[0], solution_newton[1], color='red', label='Metoda Newtona')
-ax.scatter(solution_newton_raphson[0], solution_newton_raphson[1], color='blue', label='Metoda Newtona-Raphsona')
-ax.set_xlabel('x1')
-ax.set_ylabel('x2')
-ax.set_title('Rozwiązanie układu równań')
-ax.legend()
-equation_text = r'$f(x) = \frac{{\sin(x^2) \cos(x)}}{{1 - x^2}}$'
-plt.text(-7, 9, equation_text, fontsize=12, )
+x1 = np.linspace(-5, 5, 100)
+x2 = np.linspace(-5, 5, 100)
 
-plt.text(-7, 8, 'Przedział: [-5, 5]', fontsize=12)
+X1, X2 = np.meshgrid(x1, x2)
+
+# Tworzymy wykres 3D
+fig = plt.figure(figsize=(12, 6))
+
+ax1 = fig.add_subplot(121, projection='3d')
+ax1.plot_surface(X1, X2, f1(X1, X2))
+ax1.set_title('$f_{1}(x_{1}, x_{2})$')
+
+ax2 = fig.add_subplot(122, projection='3d')
+ax2.plot_surface(X1, X2, f2(X1, X2))
+ax2.set_title('$f_{2}(x_{1}, x_{2})$')
+
+# Dodanie równania do wykresu
+equation_text_1 = r"$x_{1} = 3x_{2} + 2,$"
+equation_text_2 = r"$x_{2} = x_{1}^{3} - 4.$"
+
+plt.figtext(0.44, 0.08, '{', ha="center", fontsize=36, va="center")
+plt.figtext(0.507, 0.10, equation_text_1, ha="center", fontsize=15, va="center")
+plt.figtext(0.5, 0.05, equation_text_2, ha="center", fontsize=15, va="center")
+
+# Dodanie rozwiązania do wykresu
+solution_text = f"Rozwiązanie: $x_{{1}}$={solution[0]:.3f}, $x_{{2}}$={solution[1]:.3f}"
+plt.figtext(0.5, 0.001, solution_text, ha="center", fontsize=15, va="center")
 
 plt.show()
