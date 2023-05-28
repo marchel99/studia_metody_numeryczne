ac6cbcd96388204a1a1123b02305ac3b3b048039
marchel99 <kubamarchelski@live.com>
2023-05-28 23:51:00 +0200

    modified:   na_kolosa.py

--- na_kolosa.py
+++ na_kolosa.py
@@ -1,4 +1,6 @@
 import sympy as sp
+import numpy as np
+import matplotlib.pyplot as plt
 
 def find_extrema(f):
     x = sp.symbols('x')
@@ -16,3 +18,17 @@ if __name__ == "__main__":
     print("Potencjalne punkty ekstremalne:")
     for ext in extrema:
         print("x =", ext)
+
+    x_vals = np.linspace(-10, 10, 1000)
+    y_vals = f(x_vals)
+
+    plt.plot(x_vals, y_vals, label='f(x) = x^3 + x + 4')
+    plt.scatter(extrema, [f(x) for x in extrema], color='red', label='Potencjalne punkty ekstremalne')
+    plt.axhline(0, color='black', linewidth=0.5)
+    plt.axvline(0, color='black', linewidth=0.5)
+    plt.legend()
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    plt.title('Wykres funkcji')
+    plt.grid(True)
+    plt.show()
