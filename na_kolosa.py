245a1f19b09ba0b4e59734c2f1e9873241d96c8d
marchel99 <kubamarchelski@live.com>
2023-05-28 23:54:17 +0200

    modified:   na_kolosa.py

--- na_kolosa.py
+++ na_kolosa.py
@@ -1,39 +1,28 @@
-import sympy as sp
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import minimize_scalar
 
-def find_extrema(f):
-    x = sp.symbols('x')
-    f_prime = sp.diff(f, x)  # Obliczenie pochodnej funkcji
+def f(x):
+    return x**3 + x + 4
 
-    extrema = sp.solve(f_prime, x)  # Znalezienie miejsc zerowych pochodnej
+# Znalezienie punktu ekstremalnego
+result = minimize_scalar(f)
+extremum = result.x
 
-    return extrema
+print("Potencjalny punkt ekstremalny:")
+print("x =", extremum)
 
-if __name__ == "__main__":
-    x = sp.symbols('x')
-    f = x**3 + x + 4
+# Wygenerowanie wykresu
+x_vals = np.linspace(-10, 10, 1000)
+y_vals = f(x_vals)
 
-    extrema = find_extrema(f)
-
-    print("Potencjalne punkty ekstremalne:")
-    for ext in extrema:
-        if sp.im(ext) == 0:  # Sprawdzenie, czy część urojona jest równa 0
-            print("x =", ext)
-
-    f_lambda = sp.lambdify(x, f, modules='numpy')  # Funkcja lambda dla funkcji f
-
-    x_vals = np.linspace(-10, 10, 1000)
-    y_vals = f_lambda(x_vals)
-
-    plt.plot(x_vals, y_vals, label='f(x) = x^3 + x + 4')
-    plt.scatter([sp.re(x) for x in extrema if sp.im(x) == 0], [f_lambda(x) for x in extrema if sp.im(x) == 0],
-                color='red', label='Potencjalne punkty ekstremalne')
-    plt.axhline(0, color='black', linewidth=0.5)
-    plt.axvline(0, color='black', linewidth=0.5)
-    plt.legend()
-    plt.xlabel('x')
-    plt.ylabel('f(x)')
-    plt.title('Wykres funkcji')
-    plt.grid(True)
-    plt.show()
+plt.plot(x_vals, y_vals, label='f(x) = x^3 + x + 4')
+plt.scatter(extremum, f(extremum), color='red', label='Potencjalny punkt ekstremalny')
+plt.axhline(0, color='black', linewidth=0.5)
+plt.axvline(0, color='black', linewidth=0.5)
+plt.legend()
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title('Wykres funkcji')
+plt.grid(True)
+plt.show()
