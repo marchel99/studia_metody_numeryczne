8575948a87111caa68ec0f3d45437207bc516133
marchel99 <kubamarchelski@live.com>
2023-05-28 23:50:39 +0200

    modified:   na_kolosa.py

--- na_kolosa.py
+++ na_kolosa.py
@@ -1,29 +1,18 @@
-def f(x):
-    return x**3 + x + 4
+import sympy as sp
 
-def bisection_method(f, a, b, tolerance=1e-6, max_iterations=100):
-    if f(a) * f(b) >= 0:
-        print("Nie można zagwarantować istnienia miejsca zerowego w danym przedziale.")
-        return None
+def find_extrema(f):
+    x = sp.symbols('x')
+    f_prime = sp.diff(f, x)  # Obliczenie pochodnej funkcji
 
-    for _ in range(max_iterations):
-        c = (a + b) / 2
+    extrema = sp.solve(f_prime, x)  # Znalezienie miejsc zerowych pochodnej
 
-        if abs(f(c)) < tolerance:
-            return c
-
-        if f(a) * f(c) < 0:
-            b = c
-        else:
-            a = c
-
-    print("Nie osiągnięto zbieżności po zadanej liczbie iteracji.")
-    return None
+    return extrema
 
 if __name__ == "__main__":
-    a = -5  # Początek przedziału
-    b = 5   # Koniec przedziału
+    f = lambda x: x**3 + x + 4
 
-    zero = bisection_method(f, a, b)
-    if zero is not None:
-        print("Miejsce zerowe: x =", zero)
+    extrema = find_extrema(f)
+
+    print("Potencjalne punkty ekstremalne:")
+    for ext in extrema:
+        print("x =", ext)
