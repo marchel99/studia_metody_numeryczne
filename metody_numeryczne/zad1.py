8398332fcb2534b22fd0084a982767c01c6627b9
marchel99 <kubamarchelski@live.com>
2023-06-13 18:58:59 +0200

    Changes to be committed: 	modified:   metody_numeryczne/zad1.py

--- metody_numeryczne/zad1.py
+++ metody_numeryczne/zad1.py
@@ -1,11 +1,11 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import scipy.integrate as spi
+from scipy.integrate import quad
 
 # Definicja funkcji
-funkcje = [(lambda x: -2 * x**3 - 2 * x - 2, -2, 2, "-2x^3 - 2x - 2"),
-           (lambda x: -np.cos(-x/2), 0, np.pi/2, "-cos(-x/2)"),
-           (lambda x: np.log(x/3), 1, 3, "ln(x/3)")]
+a, b, c, d, e, f, g = -2, -2, -2, -1, -2, 3, 3
+funkcje = [(lambda x: c*x**3 + b*x + a, -2, 2), 
+           (lambda x: d*np.cos(x/e), 0, np.pi/2),
+           (lambda x: np.log(x/f), 1, g)]
 
 # Metoda prostokątów
 def metoda_prostokatow(funkcja, a, b, n):
@@ -15,31 +15,26 @@ def metoda_prostokatow(funkcja, a, b, n):
 # Metoda trapezów
 def metoda_trapezow(funkcja, a, b, n):
     h = (b - a) / n
-    return (0.5 * (funkcja(a) + funkcja(b)) + sum([funkcja(a + i*h) for i in range(1, n)]) ) * h
+    return (0.5 * (funkcja(a) + funkcja(b)) + sum([funkcja(a + i*h) for i in range(1, n)])) * h
 
 # Metoda Simpsona
 def metoda_simpsona(funkcja, a, b, n):
     h = (b - a) / (2*n)
     return (1/3) * h * (funkcja(a) + funkcja(b) + 4 * sum([funkcja(a + i*h) for i in range(1, 2*n, 2)]) + 2 * sum([funkcja(a + i*h) for i in range(2, 2*n, 2)]))
 
-# Tworzenie wykresów
-N = range(1, 101)
+# Obliczenia i wypisanie wyników
+liczba_podprzedzialow = [10, 20, 50, 100]
 
-for funkcja, a, b, label in funkcje:
-    dokladna_wartosc, _ = spi.quad(funkcja, a, b)
-    
-    bledy_prostokatow = [abs(metoda_prostokatow(funkcja, a, b, n) - dokladna_wartosc) for n in N]
-    bledy_trapezow = [abs(metoda_trapezow(funkcja, a, b, n) - dokladna_wartosc) for n in N]
-    bledy_simpsona = [abs(metoda_simpsona(funkcja, a, b, n) - dokladna_wartosc) for n in N]
-    
-    plt.figure()
-    plt.plot(N, bledy_prostokatow, label="Metoda prostokątów")
-    plt.plot(N, bledy_trapezow, label="Metoda trapezów")
-    plt.plot(N, bledy_simpsona, label="Metoda Simpsona")
-    plt.xlabel('Liczba podprzedziałów')
-    plt.ylabel('Błąd bezwzględny')
-    plt.title(f'Błąd bezwzględny metod całkowania dla funkcji {label}')
-    plt.legend()
-    plt.grid()
+for i, (funkcja, a, b) in enumerate(funkcje):
+    calka, _ = quad(funkcja, a, b)
+    print(f"Funkcja {i+1}:")
+    for n in liczba_podprzedzialow:
+        wynik_prostokaty = metoda_prostokatow(funkcja, a, b, n)
+        wynik_trapezy = metoda_trapezow(funkcja, a, b, n)
+        wynik_simpsona = metoda_simpsona(funkcja, a, b, n)
 
-plt.show()
+        print(f"  Liczba podprzedziałów = {n}")
+        print(f"    Metoda prostokątów:    {wynik_prostokaty}, Błąd: {abs(wynik_prostokaty - calka)}")
+        print(f"    Metoda trapezów:       {wynik_trapezy}, Błąd: {abs(wynik_trapezy - calka)}")
+        print(f"    Metoda Simpsona:       {wynik_simpsona}, Błąd: {abs(wynik_simpsona - calka)}")
+    print()
