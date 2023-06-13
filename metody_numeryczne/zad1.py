6fb4f4f815f60f899db42d0eed3428e571e6001f
marchel99 <kubamarchelski@live.com>
2023-06-13 19:43:30 +0200

    modified:   metody_numeryczne/zad1.py

--- metody_numeryczne/zad1.py
+++ metody_numeryczne/zad1.py
@@ -4,12 +4,15 @@ import matplotlib.pyplot as plt
 def f2(x):
     return -np.cos(x/-2)
 
-def rectangle_method(f, interval, n):
+def simpson_method(f, interval, n):
+    if n % 2 != 0:
+        n += 1
+
     h = (interval[1] - interval[0]) / n
-    result = 0
-    for i in range(n):
-        result += f(interval[0] + h * (i + 0.5))
-    result *= h
+    x = np.linspace(interval[0], interval[1], n+1)
+    y = f(x)
+
+    result = h / 3 * (y[0] + y[-1] + 4 * np.sum(y[1:-1:2]) + 2 * np.sum(y[2:-2:2]))
     return result
 
 # Definicja przedziału
@@ -20,17 +23,17 @@ n = 100
 x_vals = np.linspace(interval[0], interval[1], 100)
 y_vals = f2(x_vals)
 
-# Obliczenie próbek metody prostokątów
+# Obliczenie próbek metody Simpsona
 x_samples = np.linspace(interval[0], interval[1], n)
 y_samples = f2(x_samples)
 
 # Wykres
 plt.plot(x_vals, y_vals, label='Funkcja: -cos(x/(-2))')
-plt.scatter(x_samples, y_samples, color='blue', label='Próbki (Metoda prostokątów)')
+plt.scatter(x_samples, y_samples, color='green', label='Próbki (Metoda Simpsona)')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.legend()
-plt.title('Wykres przybliżenia metody prostokątów dla funkcji')
+plt.title('Wykres przybliżenia metody Simpsona dla funkcji')
 plt.grid(True)
 
 # Zaznaczanie obszarów między wykresem a osią x
