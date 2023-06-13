df89a48448167defc2698c436837afd7c082a32a
marchel99 <kubamarchelski@live.com>
2023-06-13 18:16:03 +0200

    modified:   metody_numeryczne/zad1.py

--- metody_numeryczne/zad1.py
+++ metody_numeryczne/zad1.py
@@ -1,35 +1,45 @@
 import numpy as np
-import math
+import matplotlib.pyplot as plt
+from scipy.integrate import quad
 
 # Definicja funkcji
-def f1(x, a=-2, b=-2, c=-2):
-    return c*x**3 + b*x + a
-
-def f2(x, d=-1, e=-2):
-    return d * math.cos(x/e)
-
-def f3(x, f=3):
-    return math.log(x/f)
+a, b, c, d, e, f, g = -2, -2, -2, -1, -2, 3, 3
+functions = [(lambda x: c*x**3 + b*x + a, -2, 2), 
+             (lambda x: d*np.cos(x/e), 0, np.pi/2),
+             (lambda x: np.log(x/f), 1, g)]
 
 # Metoda prostokątów
-def rectangle_method(f, a, b, n):
+def rectangle_rule(func, a, b, n):
     h = (b - a) / n
-    return h * sum(f(a + i*h) for i in range(n))
+    return sum([func(a + i*h) for i in range(n)]) * h
 
 # Metoda trapezów
-def trapezoid_method(f, a, b, n):
+def trapezoidal_rule(func, a, b, n):
     h = (b - a) / n
-    return h * (0.5*f(a) + 0.5*f(b) + sum(f(a + i*h) for i in range(1, n)))
+    return (0.5 * (func(a) + func(b)) + sum([func(a + i*h) for i in range(1, n)])) * h
 
 # Metoda Simpsona
-def simpson_method(f, a, b, n):
-    h = (b - a) / n
-    return h / 3 * (f(a) + f(b) + sum((4 if i % 2 == 1 else 2) * f(a + i*h) for i in range(1, n)))
+def simpson_rule(func, a, b, n):
+    h = (b - a) / (2*n)
+    return (1/3) * h * (func(a) + func(b) + 4 * sum([func(a + i*h) for i in range(1, 2*n, 2)]) + 2 * sum([func(a + i*h) for i in range(2, 2*n, 2)]))
 
-# Testowanie metod
-for n in [10, 100, 1000, 10000]:
-    print(f"n = {n}")
-    for f, a, b in [(f1, -2, 2), (f2, 0, math.pi/2), (f3, 1, 3)]:
-        print(f"Rectangle: {rectangle_method(f, a, b, n)}")
-        print(f"Trapezoid: {trapezoid_method(f, a, b, n)}")
-        print(f"Simpson: {simpson_method(f, a, b, n)}")
+# Wykres
+x = np.arange(1, 100)
+for i, (func, a, b) in enumerate(functions):
+    integral, _ = quad(func, a, b)
+    y1 = [abs(rectangle_rule(func, a, b, n) - integral) for n in x]
+    y2 = [abs(trapezoidal_rule(func, a, b, n) - integral) for n in x]
+    y3 = [abs(simpson_rule(func, a, b, n) - integral) for n in x]
+
+    plt.figure()
+    plt.plot(x, y1, label="Rectangle rule")
+    plt.plot(x, y2, label="Trapezoidal rule")
+    plt.plot(x, y3, label="Simpson's rule")
+    plt.legend()
+    plt.xlabel("Number of subintervals")
+    plt.ylabel("Error")
+    plt.title(f"Error comparison for function {i+1}")
+    plt.yscale("log")
+    plt.grid()
+
+plt.show()
