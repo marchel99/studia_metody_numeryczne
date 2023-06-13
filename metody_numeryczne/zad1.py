5e62d26b30d97b17c10ab85da5db4202d0313a29
marchel99 <kubamarchelski@live.com>
2023-06-13 19:07:37 +0200

    modified:   metody_numeryczne/zad1.py

--- metody_numeryczne/zad1.py
+++ metody_numeryczne/zad1.py
@@ -1,40 +1,32 @@
 import numpy as np
-from scipy.integrate import quad
+import matplotlib.pyplot as plt
 
-# Definicja funkcji
-a, b, c, d, e, f, g = -2, -2, -2, -1, -2, 3, 3
-funkcje = [(lambda x: c*x**3 + b*x + a, -2, 2), 
-           (lambda x: d*np.cos(x/e), 0, np.pi/2),
-           (lambda x: np.log(x/f), 1, g)]
+def f1(x):
+    return -2*x**3 - 2*x - 2
 
-# Metoda prostokątów
-def metoda_prostokatow(funkcja, a, b, n):
-    h = (b - a) / n
-    return sum([funkcja(a + i*h) for i in range(n)]) * h
+def rectangle_method(f, range, n):
+    h = (range[1] - range[0]) / n
+    result = 0
+    for i in range(n):
+        result += f(range[0] + h * (i + 0.5))
+    result *= h
+    return result
 
-# Metoda trapezów
-def metoda_trapezow(funkcja, a, b, n):
-    h = (b - a) / n
-    return (0.5 * (funkcja(a) + funkcja(b)) + sum([funkcja(a + i*h) for i in range(1, n)])) * h
+# Definicja przedziału
+range = [-2, 2]
+n = 100
 
-# Metoda Simpsona
-def metoda_simpsona(funkcja, a, b, n):
-    h = (b - a) / (2*n)
-    return (1/3) * h * (funkcja(a) + funkcja(b) + 4 * sum([funkcja(a + i*h) for i in range(1, 2*n, 2)]) + 2 * sum([funkcja(a + i*h) for i in range(2, 2*n, 2)]))
+# Obliczenie wartości przybliżonej
+x_vals = np.linspace(range[0], range[1], n+1)
+y_vals = f1(x_vals)
+approx_vals = np.ones_like(x_vals) * rectangle_method(f1, range, n)
 
-# Obliczenia i wypisanie wyników
-liczba_podprzedzialow = [10, 20, 50, 100]
-
-for i, (funkcja, a, b) in enumerate(funkcje):
-    calka, _ = quad(funkcja, a, b)
-    print(f"Funkcja {i+1}:")
-    for n in liczba_podprzedzialow:
-        wynik_prostokaty = metoda_prostokatow(funkcja, a, b, n)
-        wynik_trapezy = metoda_trapezow(funkcja, a, b, n)
-        wynik_simpsona = metoda_simpsona(funkcja, a, b, n)
-
-        print(f"  Liczba podprzedziałów = {n}")
-        print(f"    Metoda prostokątów:    {wynik_prostokaty}, Błąd: {abs(wynik_prostokaty - calka)}")
-        print(f"    Metoda trapezów:       {wynik_trapezy}, Błąd: {abs(wynik_trapezy - calka)}")
-        print(f"    Metoda Simpsona:       {wynik_simpsona}, Błąd: {abs(wynik_simpsona - calka)}")
-    print()
+# Wykres
+plt.plot(x_vals, y_vals, label='Funkcja')
+plt.plot(x_vals, approx_vals, '--', label='Metoda prostokątów (n=100)')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.title('Wykres przybliżenia metody prostokątów dla funkcji 1')
+plt.grid(True)
+plt.show()
