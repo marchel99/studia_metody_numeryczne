e1b39ec517a0d81876540e9f570a3dc4d25cb52d
marchel99 <kubamarchelski@live.com>
2023-06-13 17:59:15 +0200

    modified:   metody_numeryczne/zad1_3.py

--- metody_numeryczne/zad1_3.py
+++ metody_numeryczne/zad1_3.py
@@ -1,38 +1,63 @@
 import numpy as np
+import math
 import scipy.integrate
+import matplotlib.pyplot as plt
 
 # Definicja funkcji
 def f1(x, a=-2, b=-2, c=-2):
     return c*x**3 + b*x + a
 
 def f2(x, d=-1, e=-2):
-    return d * np.cos(x/e)
+    return d * math.cos(x/e)
 
 def f3(x, f=3):
-    return np.log(x/f)
+    return math.log(x/f)
 
-# Przedziały całkowania
-intervals = [(-2, 2), (0, np.pi/2), (1, 3)]
+# Metoda prostokątów
+def rectangle_method(f, a, b, n):
+    h = (b - a) / n
+    return h * sum(f(a + i*h) for i in range(n))
 
-# Węzły i wagi dla kwadratur Gaussa-Legendre’a
-nodes_weights = [
-    ([0.0], [2.0]),
-    ([-0.5773502692, 0.5773502692], [1.0, 1.0]),
-    ([-0.7745966692, 0.0, 0.7745966692], [0.5555555556, 0.8888888889, 0.5555555556]),
-    ([-0.8611363116, 0.8611363116, -0.3399810436, 0.3399810436], [0.3478548451, 0.3478548451, 0.6521451549, 0.6521451549]),
-    ([-0.9061798459, 0.9061798459, -0.5384693101, 0.5384693101, 0.0], [0.2369268851, 0.2369268851, 0.4786286705, 0.4786286705, 0.5688888889]),
-    ([-0.9324695142, 0.9324695142, -0.6612093865, 0.6612093865, -0.2386191861, 0.2386191861], [0.1713244924, 0.1713244924, 0.3607615730, 0.3607615730, 0.4679139346, 0.4679139346]),
-    ([-0.9491079123, 0.9491079123, -0.7415311856, 0.7415311856, -0.4058451514, 0.4058451514, 0.0], [0.1294849662, 0.1294849662, 0.2797053915, 0.2797053915, 0.3818300505, 0.3818300505, 0.4179591837]),
-    ([-0.9602898565, 0.9602898565, -0.7966664774, 0.7966664774, -0.5255324099, 0.5255324099, -0.1834336425, 0.1834336425], [0.1012285363, 0.1012285363, 0.2223810345, 0.2223810345, 0.3137066459, 0.3137066459, 0.3626837834, 0.3626837834])
-]
+# Metoda trapezów
+def trapezoid_method(f, a, b, n):
+    h = (b - a) / n
+    return h * (0.5*f(a) + 0.5*f(b) + sum(f(a + i*h) for i in range(1, n)))
 
-# Obliczanie całek za pomocą kwadratur Gaussa-Legendre’a
-for i, (nodes, weights) in enumerate(nodes_weights, start=1):
-    print(f"For n={i}:")
-    for j, (f, (a, b)) in enumerate(zip([f1, f2, f3], intervals), start=1):
-        # Przeprowadzenie mapowania przedziałów całkowania na przedział [-1, 1]
-        nodes_transformed = [(b - a) / 2 * x + (a + b) / 2 for x in nodes]
-        weights_transformed = [(b - a) / 2 * w for w in weights]
-        # Obliczenie całki
-        result = sum(w * f(x) for x, w in zip(nodes_transformed, weights_transformed))
-        print(f"  Result for function {j}: {result}")
+# Metoda Simpsona
+def simpson_method(f, a, b, n):
+    h = (b - a) / n
+    return h / 3 * (f(a) + f(b) + sum((4 if i % 2 == 1 else 2) * f(a + i*h) for i in range(1, n)))
+
+# Testowanie metod
+for n in [10, 100, 1000, 10000]:
+    print(f"n = {n}")
+    for f, a, b in [(f1, -2, 2), (f2, 0, math.pi/2), (f3, 1, 3)]:
+        print(f"Rectangle: {rectangle_method(f, a, b, n)}")
+        print(f"Trapezoid: {trapezoid_method(f, a, b, n)}")
+        print(f"Simpson: {simpson_method(f, a, b, n)}")
+
+# Metoda Romberga dla funkcji f2
+a = 0
+b = np.pi/2
+result = scipy.integrate.romberg(f2, a, b, show=True)
+print(f"Romberg result for f2: {result}")
+
+# Wykresy
+x = np.linspace(-2, 2, 1000)
+plt.figure(figsize=(12, 8))
+
+plt.subplot(2, 2, 1)
+plt.plot(x, f1(x))
+plt.title('f1')
+
+plt.subplot(2, 2, 2)
+plt.plot(x, f2(x))
+plt.title('f2')
+
+x = np.linspace(1, 3, 1000)
+plt.subplot(2, 2, 3)
+plt.plot(x, f3(x))
+plt.title('f3')
+
+plt.tight_layout()
+plt.show()
